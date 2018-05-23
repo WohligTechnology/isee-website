d735b9648ed83433eb14f0189dd7d73daba65d5b
snehalthakare <snehal.thakare@wohlig.com>
2018-05-23 13:07:08 +0530

    chanages

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,39 +1,46 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-        name: "Home",
-        classis: "active",
-        anchor: "home",
-        subnav: [
-            //     {
-            //     name: "Subnav1",
-            //     classis: "active",
-            //     anchor: "home"
-            // }
-        ]
+            name: "Home",
+            classis: "active",
+            anchor: "home",
+            subnav: [
+                //     {
+                //     name: "Subnav1",
+                //     classis: "active",
+                //     anchor: "home"
+                // }
+            ]
 
-    }, 
-    // {
-    //     name: "About Us",
-    //     classis: "active",
-    //     anchor: "about-us",
-    //     subnav: []
-    // }, {
-    //     name: "Team",
-    //     classis: "active",
-    //     anchor: "our-team",
-    //     subnav: []
-    // }, 
-    {
-        name: "Isee solution",
-        classis: "active",
-        anchor: "why-isee",
-        subnav: []
-    }, {
-        name: "Contact Us",
-        classis: "active",
-        anchor: "contact-us",
-        subnav: []
-    }];
+        },
+        // {
+        //     name: "About Us",
+        //     classis: "active",
+        //     anchor: "about-us",
+        //     subnav: []
+        // }, {
+        //     name: "Team",
+        //     classis: "active",
+        //     anchor: "our-team",
+        //     subnav: []
+        // }, 
+        {
+            name: "Isee solution",
+            classis: "active",
+            anchor: "why-isee",
+            subnav: []
+        }, {
+            name: "FEATURES OF iSEE",
+            classis: "active",
+            anchor: "featuresofisee",
+            subnav: []
+        },
+        {
+            name: "Contact Us",
+            classis: "active",
+            anchor: "contact-us",
+            subnav: []
+        }
+    ];
 
     return {
         getNavigation: function () {
